05b94912c2f3a3364e7194c573a61cf50eda5fca
abdolm <sasanabdolm@gmail.com>
2023-06-29 16:39:46 +0200

    dernier

--- Bulky/Controllers/CategoryController.cs
+++ Bulky/Controllers/CategoryController.cs
@@ -24,10 +24,15 @@ namespace Bulky.Controllers
         }
         [HttpPost]
         public IActionResult Create(Category obj)
-        {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+        {   
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+            
         }
     }
 }
--- Bulky/Models/Category.cs
+++ Bulky/Models/Category.cs
@@ -6,7 +6,9 @@ namespace Bulky.Models
     {
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
         public required string Name { get; set; }
+        [Range(1, 100)]
         public int DisplayOrder { get; set; }
     }
 }
